1fe1dffc0ff2617063eeabbe6099b65d6761b968
Jim Shaw <jimxshaw@gmail.com>
2015-10-12 16:08:11 -0400

    added IOrderRepository interface

---
+++ SGCorporation/SGCorporation.Models/OrderSlip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCorporation.Models
+{
+    public class OrderSlip
+    {
+        public DateTime OrderDate { get; set; }
+        public Order Order { get; set; }
+    }
+}
