316c3064f9463b813eecdf7b7371a39500c9960b
koshin abdi <kabdi8@gmail.com>
2015-10-13 10:05:25 -0400

    added factory with option for mode

---
+++ SGCorporation/SGCorporation.Data/OrderRepositoryFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using SGCorporation.Models.Interfaces;
+
+namespace SGCorporation.Data
+{
+    public class OrderRepositoryFactory
+    {
+        private static string _option = ConfigurationManager.AppSettings["Option"];
+
+        public static IOrderRepository CreateOrderRepository()
+        {
+
+            switch (_option)
+            {
+                case "Test":
+                    return new TestOrderRepository();
+                    
+                case "Prod":
+                    return new OrderRepository();
+                default:
+                    throw new Exception(string.Format("{0} not supported!"));
+
+
+            }
+
+        }
+    }
+}
