5f88b0f39d8fb9bf932cdae4019fdd37c313893a
koshin abdi <kabdi8@gmail.com>
2015-10-14 14:09:54 -0400

    updating edit

--- SGCorporation/SGCorporation.UI/Workflows/EditOrderWorkflow.cs
+++ SGCorporation/SGCorporation.UI/Workflows/EditOrderWorkflow.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SGCorporation.BLL;
+using SGCorporation.Models;
 
 namespace SGCorporation.UI.Workflows
 {
@@ -10,13 +13,63 @@ namespace SGCorporation.UI.Workflows
     {
         public void Execute()
         {
-            DisplayOrdersWorkflow dow = new DisplayOrdersWorkflow();
-            dow.Execute();
+            Console.Clear();
+
+            DateTime currentDate = PromptForDate();
+            int orderNo = PromptForOrderNo();
+
+            OrderOperations ops = new OrderOperations();
+
+            var order = ops.GetOrderNo(currentDate, orderNo);
+
+            order.CustomerName = PromptToEditStrings("CustomerName");
+           
+
+            ops.EditOrder(currentDate, order);
 
 
-            
+
+        }
+        public DateTime PromptForDate()
+        {
+            Console.Write("Enter the date for your Order Date (MMDDYYYY): ");
+            string input = Console.ReadLine();
+
+
+            string format = "MMddyyyy";
+            DateTime date = DateTime.ParseExact(input, format, CultureInfo.InvariantCulture, DateTimeStyles.None);
+
+            return date;
         }
 
-      
+        public int PromptForOrderNo()
+        {
+            Console.Write("Enter the order number: ");
+            string input = Console.ReadLine();
+
+            int OrderNo = int.Parse(input);
+
+            return OrderNo;
+        }
+
+        public string PromptToEditStrings(string property)
+        {
+            switch (property)
+            {
+                case "CustomerName":
+                    Console.WriteLine("Enter a new customer name: ");
+                    return Console.ReadLine();
+                    
+                default:
+                    Console.WriteLine("Invalid string");
+                    return Console.ReadLine();
+                    
+
+
+
+            }
+        }
+
+
     }
 }
